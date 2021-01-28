78613d77e755606e7d504c03e773114bf2ef80b9
Kai <kimobrie@ucsc.edu>
2021-01-27 18:58:51 -0700

    bitInt written

--- assignment1/code/bigint.cpp
+++ assignment1/code/bigint.cpp
@@ -9,11 +9,26 @@ using namespace std;
 #include "bigint.h"
 
 bigint::bigint (long that): uvalue (that), is_negative (that < 0) {
-   DEBUGF ('~', this << " -> " << uvalue)
+  // DEBUGF ('~', this << " -> " << uvalue)
+   /*
+   if(is_negative == true){   //need to do anything else
+      while(that<0){          //for the sign?
+         bigint.push_back(that%0);  
+         that/10;
+      }
+   }
+
+   else{
+      while(that > 0){  //if that is negative,
+         bigint.push_back(that%0);  
+         that/10;
+      }
+   }*/
+   //dont change?
 }
 
 bigint::bigint (const ubigint& uvalue_, bool is_negative_):
-                uvalue(uvalue_), is_negative(is_negative_) {
+                uvalue(uvalue_), is_negative(is_negative_) {//dont change?
 }
 
 bigint::bigint (const string& that) {
@@ -21,38 +36,114 @@ bigint::bigint (const string& that) {
    uvalue = ubigint (that.substr (is_negative ? 1 : 0));
 }
 
-bigint bigint::operator+ () const {
+bigint bigint::operator+ () const { //returns argument
    return *this;
 }
 
-bigint bigint::operator- () const {
+bigint bigint::operator- () const { //flips the sign
    return {uvalue, not is_negative};
 }
 
 bigint bigint::operator+ (const bigint& that) const {
-   ubigint result = uvalue + that.uvalue;
-   return result;
+   bigint plus_result;
+   bool sign = false; //(positive)
+   // both negative
+   if(is_negative and is_negative){   
+      plus_result = uvalue + that.uvalue; //calls uvalue operator+
+      sign = true; //sign is true (negative)
+   }
+   // both positive
+   else if(!is_negative and !is_negative){  
+      plus_result = uvalue + that.uvalue; //calls uvalue operator+
+      sign = false;  //set sign to false 
+   }
+
+   // signs are different
+   else{
+   
+      if(uvalue > that.uvalue){ //if one is greater or equal
+         sign = is_negative; //sign = the larger sign
+         plus_result = uvalue - that.uvalue;   //subtract 
+      }
+      else if (that.uvalue > uvalue){
+         sign = that.is_negative;   //sign of bigger number
+         plus_result = that.uvalue - uvalue;   //subtract 
+      }
+      else if(that.uvalue == uvalue){  //ex: +10 -10 = 0
+         sign = false;  // 0 is positive
+         plus_result = 0;
+      }
+   }
+
+   return (plus_result, sign);   //returns appropriate bigint and sign
+
+
 }
 
 bigint bigint::operator- (const bigint& that) const {
-   ubigint result = uvalue - that.uvalue;
-   return result;
+   bigint sub_result;
+   bool sign = false; //(positive)
+
+   // both negative
+   if(is_negative and is_negative){   
+      sub_result = uvalue + that.uvalue; //calls uvalue operator+
+      sign = false; //sign is positive (negative)
+   }
+   else if(!is_negative and !is_negative){  
+      if(uvalue > that.uvalue){ //if one is greater or equal
+         sign = false; //both positive so remain positive
+         sub_result = uvalue - that.uvalue;   //subtractfrom larger 
+      }
+      else if (that.uvalue > uvalue){
+         sign = true;   // 1-2 = -1
+         sub_result = that.uvalue - uvalue;   //subtract from larger
+      }
+      else if(that.uvalue == uvalue){  //ex: +10 -10 = 0
+         sign = false;  // 0 is positive
+         sub_result = 0;
+      }
+   }
+   else{ //signs are different
+      if(uvalue > that.uvalue){ //if one is greater or equal
+         sign = is_negative; //sign of uvalue
+         sub_result = uvalue + that.uvalue;   //subtract from larger 
+      }
+      else if (that.uvalue > uvalue){  //that.uvalue can be pos or neg
+         sign = that.is_negative;   //sign of that.uvalue
+         sub_result = that.uvalue + uvalue;   //subtract from larger
+      }
+   }
+   return (sub_result , sign);
 }
 
 
 bigint bigint::operator* (const bigint& that) const {
-   bigint result = uvalue * that.uvalue;
-   return result;
+   bool sign = true;
+   if(is_negative == that.is_negative){
+      sign = false;
+   }
+   bigint times_result = uvalue * that.uvalue;
+   
+
+   return (times_result , sign);
 }
 
 bigint bigint::operator/ (const bigint& that) const {
-   bigint result = uvalue / that.uvalue;
-   return result;
+   bool sign = true;
+   if(is_negative == that.is_negative){
+      sign = false;
+   }
+   bigint div_result = uvalue / that.uvalue;
+   return (div_result,sign);
 }
 
 bigint bigint::operator% (const bigint& that) const {
-   bigint result = uvalue % that.uvalue;
-   return result;
+   bool sign = true;
+   if(is_negative == that.is_negative){
+      sign = false;
+   }
+   bigint mod_result = uvalue % that.uvalue;
+   return (mod_result,sign);
 }
 
 bool bigint::operator== (const bigint& that) const {
