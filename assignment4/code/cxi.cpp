fad8b0b5f37f7723283e320db396a3cb06754e3b
Kai <kimobrie@ucsc.edu>
2021-03-09 22:53:28 -0700

    get done, put partway done

--- assignment4/code/cxi.cpp
+++ assignment4/code/cxi.cpp
@@ -5,10 +5,13 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <fstream>
+
 using namespace std;
 
 #include <libgen.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include "protocol.h"
@@ -22,6 +25,12 @@ unordered_map<string,cxi_command> command_map {
    {"exit", cxi_command::EXIT},
    {"help", cxi_command::HELP},
    {"ls"  , cxi_command::LS  },
+   {"get" , cxi_command::GET },
+   {"put" , cxi_command::PUT },
+   {"rm"  , cxi_command::RM  },
+
+
+
 };
 
 static const char help[] = R"||(
@@ -56,11 +65,74 @@ void cxi_ls (client_socket& server) {
       cout << buffer.get();
    }
 }
+void cxi_get (client_socket& server, vector<string>& splitvec) {
+   cxi_header header;
+   header.command = cxi_command::GET;
+   strncpy(header.filename,splitvec[1].c_str(),splitvec.size());
+   
+   outlog << "sending header " << header << endl;
+   send_packet (server, &header, sizeof header);
+   recv_packet (server, &header, sizeof header);
+   outlog << "received header " << header << endl;
+   //if(!fileout) error
+   if (header.command != cxi_command::FILEOUT) {
+      outlog << "sent GET, server did not return FILEOUT" << endl;
+      outlog << "server returned " << header << endl;
+   }
+   //else set nbytes, declare buffer, recv, writefile,log<<success
+   else {
+      size_t host_nbytes = ntohl (header.nbytes);
+      auto buffer = make_unique<char[]> (host_nbytes + 1);
+      recv_packet (server, buffer.get(), host_nbytes);
+   //ged rid of outlogs outlog << "received " << host_nbytes << " bytes" << endl;
+      buffer[host_nbytes] = '\0';
+      cout << buffer.get();
+   //create ofstream and write 
+   ofstream write_file (header.filename);
+   write_file.write(buffer.get(),host_nbytes);
+   write_file.close();
+   write_file << "GET sucess" << endl;
+   }
+   
+}
+void cxi_put (client_socket& server, vector<string>& splitvec) {
+   cxi_header header;
+   header.command = cxi_command::PUT;
+   
+   strncpy(header.filename,splitvec[1].c_str(),splitvec.size());
+   //if !file.exist ->error
+   //use ifstream
+
+   ifstream read_file (header.filename);
+   
+   if(read_file){
+      cerr<< header.filename << " does not exist" << endl
+      return;
+   }
+   
+}
+
 
 
 void usage() {
    cerr << "Usage: " << outlog.execname() << " [host] [port]" << endl;
    throw cxi_exit();
+}
+//taken from split function in asg2
+vector<string> split (const string& line, const string& delimiters) {
+   vector<string> words;
+   size_t end = 0;
+
+   // Loop over the string, splitting out words, and for each word
+   // thus found, append it to the output wordvec.
+   for (;;) {
+      size_t start = line.find_first_not_of (delimiters, end);
+      if (start == string::npos) break;
+      end = line.find_first_of (delimiters, start);
+      words.push_back (line.substr (start, end - start));
+   }
+  // DEBUGF ('u', words);
+   return words;
 }
 
 int main (int argc, char** argv) {
@@ -77,22 +149,33 @@ int main (int argc, char** argv) {
       outlog << "connected to " << to_string (server) << endl;
       for (;;) {
          string line;
-         getline (cin, line);
+         getline (cin, line); //split this line after if
          if (cin.eof()) throw cxi_exit();
+         vector<string> splitline = split(line," ");//makes new vector
          outlog << "command " << line << endl;
-         const auto& itor = command_map.find (line);
+         const auto& itor = command_map.find (splitline[0]);
          cxi_command cmd = itor == command_map.end()
                          ? cxi_command::ERROR : itor->second;
-         switch (cmd) {
+         switch (cmd) { //add appropriate switch cases
+         //take split funciton from util.cpp asg2
             case cxi_command::EXIT:
                throw cxi_exit();
                break;
             case cxi_command::HELP:
                cxi_help();
                break;
-            case cxi_command::LS:
+            case cxi_command::LS:   
                cxi_ls (server);
                break;
+            case cxi_command::GET:   
+               cxi_get (server, splitline);
+               break;
+            case cxi_command::PUT:   
+               cxi_put (server, splitline);
+               break;
+            case cxi_command::RM:   
+               cxi_rm (server, splitline);
+               break;   
             default:
                outlog << line << ": invalid command" << endl;
                break;
