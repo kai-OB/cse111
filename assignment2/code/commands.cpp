8af7b90649a7022c71f7ea38b31e7a067957d2c2
Kai <kimobrie@ucsc.edu>
2021-05-02 17:53:35 -0700

    jsut need to make cat work for paths but make and mkdir work perfectly :))

--- assignment2/code/commands.cpp
+++ assignment2/code/commands.cpp
@@ -47,6 +47,7 @@ void fn_cat (inode_state& state, const wordvec& words) {
    //reported if no files are specified, a file does not exist, or is a directory.
    if(words.size() == 1){   //if no files are specified
       throw command_error ("cat: No files are specified"); //dont work
+      // program needs to continue!!!!!!
    }
    else{
       for(unsigned long i = 1;i < words.size(); ++i){
@@ -56,7 +57,7 @@ void fn_cat (inode_state& state, const wordvec& words) {
             throw command_error("cat: "+ words.at(i) +": No such file or directory");
          }
          if(state_dir->is_dir_(words.at(i))==true){//everything is directory?
-            throw command_error("cat: "+ words.at(i) +": is a directory!");   //throw command error not cerr or cout
+            throw command_error("cat: "+ words.at(i) +": Is a directory");   //throw command error not cerr or cout
          }
          /*if((words.at(i))=="/"){//everything is directory?
             throw command_error("cat: "+ words.at(i) +": is a directory!");   //throw command error not cerr or cout
@@ -104,24 +105,56 @@ void fn_make (inode_state& state, const wordvec& words) {
    if(words.size()==1){
       throw command_error ("make: No target specified"); //dont work
    } 
-   else if(state_dir->file_dne(words.at(1))==true){//this works
-      //if file dne then can do normal cause isnt dir or file
-      inode_ptr new_file = state.get_cwd()->get_contents()->mkfile(words.at(1));
-      //+2 makes it not include make or filename, jsut contents
-      new_file->get_contents()->writefile(wordvec(words.begin()+2,words.end())); 
+   wordvec split_path = split(words.at(1),"/");//skips make call but includes all paths
+   int count = 1;
+   //is a path
+   if(split_path.size()>1){//if its a path
+      for(auto i =split_path.begin()+1; i< split_path.end()-1;i++){ //skip last cause creating last so wont exist
+         
+         if(state_dir->file_dne(split_path.at(count)) == true){
+            throw command_error ("make: "+split_path.at(count)+": Directory does not exist"); //dont work
+            //like foo/wrongdir/bar/newfile
+            //would output wrongdir: no such file or dir
+         }
+         else if(state_dir->is_dir_(split_path.at(count)) == false){
+            throw command_error ("make: "+split_path.at(count)+": Directory does not exist"); //dont work
+         }
+         count++; //cause the auto is an itr
+      }
+      if(state_dir->file_dne(split_path.at(count))==false){//this works
+         if(state_dir->is_dir_(split_path.at(count)) == true){
+            throw command_error ("make: "+ words.at(1) +": Is a directory"); //dont work
+         }
+         else
+         {  //updates file if exists
+            inode_ptr updated_pointer = state_dir->update_file(split_path.at(count),wordvec(words.begin()+2,words.end()));
+         }
+         
+      }
+      else{
+         inode_ptr new_file = state.get_cwd()->get_contents()->mkfile(split_path.at(count));
+         //+2 makes it not include make or filename, jsut contents
+         new_file->get_contents()->writefile(wordvec(words.begin()+2,words.end())); 
+      }
    }
-
-   else if(state_dir->is_dir_(words.at(1))==true){
-      throw command_error("cat: "+ words.at(1) +": is a directory!");   //throw command error
+   //no path, this works!
+   else{
+     if(state_dir->file_dne(words.at(1))==true){//this works
+         //if file dne then can do normal cause isnt dir or file
+         inode_ptr new_file = state.get_cwd()->get_contents()->mkfile(words.at(1));
+         //+2 makes it not include make or filename, jsut contents
+         new_file->get_contents()->writefile(wordvec(words.begin()+2,words.end())); 
+      }
+      else{//not dir, but updating existing file
+         if(state_dir->is_dir_(words.at(1))==true){//this works
+             throw command_error ("make: "+ words.at(1) +": Is a directory"); 
+         }
+         else{
+            inode_ptr updated_pointer = state_dir->update_file(words.at(1),wordvec(words.begin()+2,words.end()));
+         // cout<< updated_pointer->get_contents()->readfile();  //makes the file, but includes "make"??
+         }
+      }
    }
-  /* else if((words.at(1))=="/"){//everything is directory?
-      throw command_error("cat: "+ words.at(1) +": is a directory!");   //throw command error 
-   }*/
-   else{//not dir, but updating existing file
-         inode_ptr updated_pointer = state_dir->update_file(words.at(1),wordvec(words.begin()+2,words.end()));
-        // cout<< updated_pointer->get_contents()->readfile();  //makes the file, but includes "make"??
-    }
-
 }
 
 void fn_mkdir (inode_state& state, const wordvec& words) {
@@ -141,12 +174,14 @@ void fn_mkdir (inode_state& state, const wordvec& words) {
       if(split_path.size()>1){//if its a path
          for(auto i =split_path.begin()+1; i< split_path.end()-1;i++){ //skip last cause creating last so wont exist
             if(state_dir->file_dne(split_path.at(count))==true){//this works //everything needs to exist but the last one
-               throw command_error ("mkdir:"+split_path.at(count)+" directory does not exist"); //dont work
+               throw command_error ("mkdir: "+split_path.at(count)+": Directory does not exist"); //dont work
             }
             count++; //cause the auto is an itr
          }
          if(state_dir->file_dne(split_path.at(count)) == false){
-            throw command_error ("mkdir:"+split_path.at(count)+" directory already exists"); //dont work
+            if(state_dir->is_dir_(split_path.at(count)) == true){
+               throw command_error ("mkdir: "+split_path.at(count)+": Directory already exists"); //dont work
+            }
          }
          else{
             state_dir->mkdir(split_path.at(count));
@@ -156,7 +191,9 @@ void fn_mkdir (inode_state& state, const wordvec& words) {
       //should be end if has path or not right?
       else{//if its not a path
          if(state_dir->file_dne(split_path.at(0)) == false){
-            throw command_error ("mkdir:"+split_path.at(0)+" directory already exists"); //dont work
+             if(state_dir->is_dir_(split_path.at(0)) == true){
+               throw command_error ("mkdir: "+split_path.at(0)+": Directory already exists"); //dont work
+             }
          }
          else{
             state_dir->mkdir(split_path.at(0));
