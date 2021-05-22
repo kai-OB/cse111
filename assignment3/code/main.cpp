f27f693a769fced15dd655af1a7e3f94606cd42e
Kai <kimobrie@ucsc.edu>
2021-05-21 23:57:59 -0700

    combined regex and catfile code

--- assignment3/code/main.cpp
+++ assignment3/code/main.cpp
@@ -21,6 +21,7 @@ using namespace std;
 #include "xpair.h"
 #include "util.h"
 
+//m.insert(xpair{key,value})
 
 using str_str_map = listmap<string,string>;
 using str_str_pair = str_str_map::value_type;
@@ -41,14 +42,13 @@ void scan_options (int argc, char** argv) {
       }
    }
 }
-
+/*
 xpair <string, str_str_pair> regex_helper( string &line){
-
    regex comment_regex {R"(^\s*(#.*)?$)"};
    regex key_value_regex {R"(^\s*(.*?)\s*=\s*(.*?)\s*$)"};
    regex trimmed_regex {R"(^\s*([^=]+?)\s*$)"};
    for (;;) {
-      //string line;
+      string line;
       getline (cin, line);
       if (cin.eof()) break;
       cout << "input: \"" << line << "\"" << endl;
@@ -64,14 +64,33 @@ xpair <string, str_str_pair> regex_helper( string &line){
          assert (false and "This can not happen.");
       }
    }
-}
+   //return 0;
+  
+}*/
 
 void catfile_helper (istream& infile, const string& filename) {
    static string colons (32, ':');
    cout << colons << endl << filename << endl << colons << endl;
+   regex comment_regex {R"(^\s*(#.*)?$)"};
+   regex key_value_regex {R"(^\s*(.*?)\s*=\s*(.*?)\s*$)"};
+   regex trimmed_regex {R"(^\s*([^=]+?)\s*$)"};
    for(;;) {
       string line;
       getline (infile, line);
+      //-----regex code
+       cout << "input: \"" << line << "\"" << endl;
+      smatch result;
+      if (regex_search (line, result, comment_regex)) {
+         cout << "Comment or empty line." << endl;
+      }else if (regex_search (line, result, key_value_regex)) {
+         cout << "key  : \"" << result[1] << "\"" << endl;
+         cout << "value: \"" << result[2] << "\"" << endl;
+      }else if (regex_search (line, result, trimmed_regex)) {
+         cout << "query: \"" << result[1] << "\"" << endl;
+      }else {
+         assert (false and "This can not happen.");
+      }
+      //-----
       if (infile.eof()) break;
       cout << line << endl;
    }
@@ -85,24 +104,24 @@ int main (int argc, char** argv) {
 //-------------------------matchlines
 const string cin_name = "-";
 int status = 0;
+   string progname ( (argv[0]));
    vector<string> filenames (&argv[1], &argv[argc]);
    if (filenames.size() == 0) filenames.push_back (cin_name);
-   for ( auto& filename: filenames) {
-      if (filename == cin_name) regex_helper (filename);
+   for (const auto& filename: filenames) {
+      if (filename == cin_name) catfile_helper (cin, filename);
       else {
          ifstream infile (filename);
          if (infile.fail()) {
             status = 1;
-            cerr << argv[0] << ": " << filename << ": "
+            cerr << progname << ": " << filename << ": "
                  << strerror (errno) << endl;
          }else {
-            regex_helper (filename);
+            catfile_helper (infile, filename);
             infile.close();
          }
       }
    }
    return status;
-   
   // cout << "EXIT_SUCCESS" << endl;
   // return EXIT_SUCCESS;
 }
